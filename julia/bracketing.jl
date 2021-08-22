8b97d1cbabe242e7f8a590e6e8636f8956fe1ef4
wegamekinglc <scrappedprince.li@gmail.com>
2021-08-22 16:10:58 +0800

    added more bisection

--- julia/bracketing.jl
+++ julia/bracketing.jl
@@ -60,7 +60,76 @@ function golden_section_search(f, a, b, n)
 end
 
 
+function quadratic_fit_search(f, a, b, c, n)
+    ya, yb, yc = f(a), f(b), f(c)
+    for i in 1:n-3
+        x = 0.5 * (ya * (b^2 - c^2) + yb * (c^2 - a^2) + yc * (a^2 - b^2)) / (ya * (b -c) + yb * (c - a) + yc * (a - b))
+        yx = f(x)
+
+        if x >= b
+            if yx > yb
+                c, yc = x, yx
+            else
+                a, ya, b, yb = b, yb, x, yx
+            end
+        elseif x < b
+            if yx > yb
+                a, ya = x, yx
+            else
+                c, yc, b, yb = b, yb, x, yx
+            end
+        end
+    end
+    return (a, b, c)
+end
+
+
+function bisection(df, a, b; ϵ=1e-14)
+    if a > b
+        a, b = b, a
+    end
+
+    ya, yb = df(a), df(b)
+    if ya == 0
+        b = a
+    end
+    if yb == 0
+        a = b
+    end
+
+    while b - a > ϵ
+        x = (a + b) / 2
+        y = df(x)
+        if y == 0
+            a, b = x, x
+        elseif y * ya > 0
+            a = x
+        else
+            b = x
+        end
+    end
+    return (a, b)
+end
+
+
+function bracket_sign_change(df, a, b; k=2)
+    if a > b
+        a, b = b, a
+    end
+
+    center, half_width = (a + b) / 2, (b - a) / 2
+    while df(a) * df(b) > 0
+        half_width *= k
+        a = center - half_width
+        b = center + half_width
+    end
+
+    return(a, b)
+end
+
+
 f(x) = (x - 3.0) ^ 2
+df(x) = 2.0 * (x - 3.0)
 
 using BenchmarkTools
 BenchmarkTools.DEFAULT_PARAMETERS.seconds = 10
--- python/bracketing.py
+++ python/bracketing.py
@@ -53,5 +53,65 @@ def golden_section_search(f, a, b, n):
     return (a, b) if a < b else (b, a)
 
 
+def quadratic_fit_search(f, a, b, c, n):
+    ya, yb, yc = f(a), f(b), f(c)
+    for i in range(1, n - 2):
+        x = 0.5 * (ya * (b**2 - c**2) + yb * (c**2 - a**2) + yc * (a**2 - b**2)) / (ya * (b -c) + yb * (c - a) + yc * (a - b))
+        yx = f(x)
+        if x > b:
+            if yx > yb:
+                c, yc = x, yx
+            else:
+                a, ya, b, yb = b, yb, x, yx
+        elif x < b:
+            if yx > yb:
+                a, ya = x, yx
+            else:
+                c, yc, b, yb = b, yb, x, yx
+    return a, b, c
+
+
+def bisection(df, a, b, ϵ=1e-14):
+    if a > b:
+        a, b = b, a
+
+    ya, yb = df(a), df(b)
+    if ya == 0:
+        b = a
+    if yb == 0:
+        a = b
+
+    while b - a > ϵ:
+        x = (a + b) / 2
+        y = df(x)
+        if y == 0:
+            a, b = x, x
+        elif y * ya > 0:
+            a = x
+        else:
+            b = x
+    return a, b
+
+
+def bracket_sign_change(df, a, b, k=2):
+    if a > b:
+        a, b = b, a
+
+    center, half_width = (a + b) / 2, (b - a) / 2
+    while df(a) * df(b) > 0:
+        half_width *= k
+        a = center - half_width
+        b = center + half_width
+    return a, b
+
+
 def f(x):
     return (x - 3.0) ** 2
+
+
+def df(x):
+    return 2. * (x - 3.0)
+
+
+if __name__ == "__main__":
+    print(quadratic_fit_search(f, 0., 2., 5., 10))
